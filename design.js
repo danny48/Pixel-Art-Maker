79a50f6f2a867013b9d74db5d556d7cf0e901505
Daniel Bohnemann <dannybohnemann@yahoo.com>
2019-07-03 21:19:47 -0500

    Update design.js

--- design.js
+++ design.js
@@ -1,39 +1,40 @@
+//is called in html code and creates the grid to users desire
 function makeGrid() {
     clearTable();
     //gets values from the input boxes and stores them in a variable
-    var rn = document.getElementById('inputWidth').value;
-    var cn = document.getElementById('inputHeight').value;
+    var column = document.getElementById('inputHeight').value;
+    var row = document.getElementById('inputWidth').value;
 
-     for(var r=0;r<parseInt(rn);r++)
-      {
-       var x=document.getElementById('pixelCanvas').insertRow(r);
-       for(var c=0;c<parseInt(cn);c++)
-        {
-         var y=  x.insertCell(c);
+    //uses the values obtained from column and loops it until the desired height is reached
+    for (var c = 0; c < parseInt(column); c++) {
+        var x = document.getElementById('pixelCanvas').insertRow(c);
+        //uses the value from the width and loops it until the desired width is reached
+        for (var r = 0; r < parseInt(row); r++) {
+            var y = x.insertCell(r);
         }
-       }
+    }
 
+    //function uses an event listener to fill in a square of the grid when clicked
+    document.querySelector("#pixelCanvas").addEventListener("click", function(event) {
+        var td = event.target;
+        while (td !== this && !td.matches("td")) {
+            td = td.parentNode;
+        }
+        if (td === this) {
+            console.log("No table cell found");
+        } else {
+            //code that fills in the square
+            td.style.backgroundColor = document.getElementById('colorPicker').value;
+        }
+    });
 
-       document.querySelector("#pixelCanvas").addEventListener("click", function(event) {
-            var td = event.target;
-            while (td !== this && !td.matches("td")) {
-                td = td.parentNode;
-            }
-            if (td === this) {
-                console.log("No table cell found");
-            } else {
-                td.style.backgroundColor = document.getElementById('colorPicker').value;
-            }
-          });
-
-       }
+}
 
 
 //function clears the table by deleting the rows and the columns in them
-    function clearTable() {
-        for(var i = document.getElementById("pixelCanvas").rows.length; i > 0;i--)
-        {
-            document.getElementById("pixelCanvas").deleteRow(i -1);
-        }
-
+function clearTable() {
+    for (var i = document.getElementById("pixelCanvas").rows.length; i > 0; i--) {
+        document.getElementById("pixelCanvas").deleteRow(i - 1);
     }
+
+}
